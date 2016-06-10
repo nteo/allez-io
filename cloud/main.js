1600bac05d841943f989ab0bfab9fc9a4a649bce
Martin Rioux <martin.rioux@nteo.ca>
2016-06-10 14:15:47 -0400

    test

--- cloud/main.js
+++ cloud/main.js
@@ -3,5 +3,25 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 Parse.Cloud.define('getCsv', function(req, res) {
-  res.success('Hi');
+  if(!req.params.start_time){
+    return res.error("start_time param not found");
+  }
+  if(!req.params.end_time){
+    return res.error("end_time param not found");
+  }
+  console.log(req.params.start_time, '---', req.params.end_time);
+  var queryMyClass = new Parse.Query("BioHex");
+  queryMyClass.limit(10);
+	queryMyClass.find(
+	{
+	    success:function(objects)
+	    {
+	        console.log("MyClass.find succeeded with objects.length = " + objects.length);
+	},
+	    error:function(error)
+	    {
+	        console.error("MyClass.find failed. error = " + error);
+	    }
+	});
+
 });
