329d08cb8c0f504812f0292ea845b6f499f771ed
Martin Rioux <martin.rioux@nteo.ca>
2016-06-10 18:16:56 -0400

    add csv parsing

--- cloud/main.js
+++ cloud/main.js
@@ -23,7 +23,7 @@ Parse.Cloud.define('getCsv', function(req, res) {
 		var csv="Heart Rate,Max Heart Rate,Average Heart Rate,Speed,Max Speed,Average Speed,Cadence,Max Cadence,Average Cadence,Power,Average Power,Max Power,Moving Time,Total Moving Time,getDistance,Total Distance,GPSStatus,GPSLatitude,"+
 		"GPSLongitude,GPSSpeed,GPSAltitude,GPSTimeUTC,GPSdateUTC,Elevation,TotalElevation,Ax,Ay,Az,Gx,Gy,Gz,Mx,My,Mz,Grade,Temperature,WindSPeed,MaxWindSpeed,AverageWindSpeed,WindDirection\n";
 		
-		
+		/*
 		for (var i = 0; i<datas.length; i++) {
 			var splitData = datas[i].sensorData.match(/.{1,16}/g);
 			for (var j = 0; j<splitData.length; j++) {
@@ -33,10 +33,11 @@ Parse.Cloud.define('getCsv', function(req, res) {
 			}
 			csv+='\n';
 		}
+		*/
 	    	//var csvBlob = json2csv(jsonBlob);
 			//res.writeHead(200, {'Content-Type': 'text/csv' });
 			//res.end('csvBlob;sssss');
-	        return res.success(objects);
+	        return res.success(datas[0].sensorData);
 	},
 	    error:function(error)
 	    {
