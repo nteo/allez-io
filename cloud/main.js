a250364304aa30a05aef65a2a37fe6a0aad0502e
Martin Rioux <martin.rioux@nteo.ca>
2016-06-10 18:14:26 -0400

    add csv parsing

--- cloud/main.js
+++ cloud/main.js
@@ -17,22 +17,22 @@ Parse.Cloud.define('getCsv', function(req, res) {
   query.greaterThanOrEqualTo( "updatedAt", new Date(req.params.start_time));
 	query.find(
 	{
-	    success:function(objects)
+	    success:function(datas)
 	    {
-	    	var datas = objects.result;
+	    	
 		var csv="Heart Rate,Max Heart Rate,Average Heart Rate,Speed,Max Speed,Average Speed,Cadence,Max Cadence,Average Cadence,Power,Average Power,Max Power,Moving Time,Total Moving Time,getDistance,Total Distance,GPSStatus,GPSLatitude,"+
 		"GPSLongitude,GPSSpeed,GPSAltitude,GPSTimeUTC,GPSdateUTC,Elevation,TotalElevation,Ax,Ay,Az,Gx,Gy,Gz,Mx,My,Mz,Grade,Temperature,WindSPeed,MaxWindSpeed,AverageWindSpeed,WindDirection\n";
 		
-		/*
+		
 		for (var i = 0; i<datas.length; i++) {
-			var splitData = datas[i].match(/.{1,16}/g);
+			var splitData = datas[i].sensorData.match(/.{1,16}/g);
 			for (var j = 0; j<splitData.length; j++) {
 				var test = Buffer(splitData[j], 'hex').readDoubleBE(0);
 				csv+=Buffer(splitData[j], 'hex').readDoubleBE(0)+',';
 				console.log(j, ' -- ', test);
 			}
 			csv+='\n';
-		}*/
+		}
 	    	//var csvBlob = json2csv(jsonBlob);
 			//res.writeHead(200, {'Content-Type': 'text/csv' });
 			//res.end('csvBlob;sssss');
