94301e37cfd66770777bc4ce8731ac341018699f
Martin Rioux <martin.rioux@nteo.ca>
2016-06-10 14:24:48 -0400

    test

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,7 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 Parse.Cloud.define('getCsv', function(req, res) {
+	console.log('trying to get ... ');
 	for (var i = req.params.length - 1; i >= 0; i--) {
 		console.log('-----', req.params[i]);
 	};
