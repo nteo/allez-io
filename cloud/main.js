c0fb287942102fcaaa14e911bd7895a2ba21bdfc
Martin Rioux <martin.rioux@nteo.ca>
2016-06-10 14:22:29 -0400

    test

--- cloud/main.js
+++ cloud/main.js
@@ -3,8 +3,11 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 Parse.Cloud.define('getCsv', function(req, res) {
+	for (var i = req.params.length - 1; i >= 0; i--) {
+		console.log('-----', req.params[i]);
+	};
   if(!req.params.start_time){
-    return res.error("start_time param not found----" +  req.params);
+    return res.error(req.params);
   }
   if(!req.params.end_time){
     return res.error("end_time param not found");
