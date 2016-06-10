411e94e928b035f49bd388f1852ca27c2f78c9d0
Martin Rioux <martin.rioux@nteo.ca>
2016-06-10 18:11:18 -0400

    add csv parsing

--- cloud/main.js
+++ cloud/main.js
@@ -36,7 +36,7 @@ Parse.Cloud.define('getCsv', function(req, res) {
 	    	//var csvBlob = json2csv(jsonBlob);
 			//res.writeHead(200, {'Content-Type': 'text/csv' });
 			//res.end('csvBlob;sssss');
-	        return res.success(objects.result);
+	        return res.success(objects);
 	},
 	    error:function(error)
 	    {
