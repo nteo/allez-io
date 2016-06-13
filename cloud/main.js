783b94c8d97b961a396a3b9d49186179cb57413a
Martin Rioux <martin.rioux@nteo.ca>
2016-06-13 09:26:27 -0400

    extract data

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 Parse.Cloud.define('getCsv', function(req, res) {
-
+	console.log("TETTETETETETTETET");
   if(!req.params.start_time){
     return res.error("no start_time");
   }
@@ -17,48 +17,46 @@ Parse.Cloud.define('getCsv', function(req, res) {
   query.lessThanOrEqualTo( "updatedAt", new Date(req.params.end_time));
   query.greaterThanOrEqualTo( "updatedAt", new Date(req.params.start_time));
   query.find().then(function(datas){
-		Parse._.each(function(result){
-        	console.log("result:", result);
-        	console.log("result:", result.sensorData);
-        
-    	});
-		return res.success(datas);
-/*	
-	{
-	    success:function(datas)
-	    {
-	    	
+	    	console.log("eee");
+	    	//console.log(datas);
 		var csv="Heart Rate,Max Heart Rate,Average Heart Rate,Speed,Max Speed,Average Speed,Cadence,Max Cadence,Average Cadence,Power,Average Power,Max Power,Moving Time,Total Moving Time,getDistance,Total Distance,GPSStatus,GPSLatitude,"+
 		"GPSLongitude,GPSSpeed,GPSAltitude,GPSTimeUTC,GPSdateUTC,Elevation,TotalElevation,Ax,Ay,Az,Gx,Gy,Gz,Mx,My,Mz,Grade,Temperature,WindSPeed,MaxWindSpeed,AverageWindSpeed,WindDirection\n";
 		
 		
 		for (var i = 0; i<datas.length; i++) {
-			console.log(datas[i]);
-			/*
-			var splitData = datas[i].result.sensorData.match(/.{1,16}/g);
+			//console.log(datas[i]);
+			
+			var splitData = datas[i].get('sensorData').match(/.{1,16}/g);
 			for (var j = 0; j<splitData.length; j++) {
 				var test = Buffer(splitData[j], 'hex').readDoubleBE(0);
 				csv+=Buffer(splitData[j], 'hex').readDoubleBE(0)+',';
-				console.log(j, ' -- ', test);
+				//console.log(j, ' -- ', test);
 			}
 			csv+='\n';
 			
 		}
-		
+		for(var p in res)
+{
+    if(typeof res[p] === "function") {
+      console.log("HIT", res[p])
+    }
+}
 	    	//var csvBlob = json2csv(jsonBlob);
 			//res.writeHead(200, {'Content-Type': 'text/csv' });
+			try {
+    			res.header("Content-Type",'application/json');
+			}
+			catch(err) {
+			    console.log(err);
+			}
+			
 			//res.end('csvBlob;sssss');
-			console.log(datas[0].sensorData);
-			console.log(datas[0].result);
-	        return res.success(datas[0]);
-	},
-	    error:function(error)
-	    {
+			console.log("*********", datas[0].get('sensorData') );
+	        return res.success(csv);
+	}, function(error){
 	        console.error("MyClass.find failed. error = " + error);
 	        return res.error(error);
-	    }
-	});
-*/
-});
-});
+	    });
 
+
+});
--- index.js
+++ index.js
@@ -4,19 +4,21 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://heroku_glg4jz31:jkssvdejvs8hpltcqkgno0vicu@ds013931.mlab.com:13931/heroku_glg4jz31';
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://heroku_glg4jz31:jkssvdejvs8hpltcqkgno0vicu@ds013931.mlab.com:13931/heroku_glg4jz31',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'allezio',
+  masterKey: process.env.MASTER_KEY || 'c5b2fe2f-b553-413f-b40b-8d2bc6bd11bd', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/api',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -31,7 +33,7 @@ var app = express();
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || '/api';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
@@ -47,9 +49,54 @@ app.get('/test', function(req, res) {
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
+
+mongodb.MongoClient.connect(databaseUri, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+  httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
+  // Initialize the app.
+  
+});
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+app.get("/userBikeDatas/:id/:start/:end", function(req, res) {
+  console.log("------",req.params.id, req.params.start, req.params.end);
+  db.collection("BioHex").find({_p_user: '_User$'+req.params.id, _updated_at:{"$gte": new Date(req.params.start),"$lte": new Date(req.params.end)}}, {sensorData:true, _updated_at:true}).toArray(function(err, datas) {
+    console.log('**',datas.length);//_p_user: '_User$WG9qNBzaVZ'
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      var csv="HeartRate,MaxHeartRate,AverageHeartRate,Speed,MaxSpeed,AverageSpeed,Cadence,MaxCadence,AverageCadence,Power,AveragePower,MaxPower,MovingTime,TotalMovingTime,getDistance,TotalDistance,GPSStatus,GPSLatitude,"+
+    "GPSLongitude,GPSSpeed,GPSAltitude,GPSTimeUTC,GPSdateUTC,Elevation,TotalElevation,Ax,Ay,Az,Gx,Gy,Gz,Mx,My,Mz,Grade,Temperature,WindSPeed,MaxWindSpeed,AverageWindSpeed,WindDirection"+"\n";
+    for (var i = 0; i<datas.length; i++) {
+      var splitData = datas[i].sensorData.match(/.{1,16}/g);
+      for (var j = 0; j<splitData.length; j++) {
+        var test = Buffer(splitData[j], 'hex').readDoubleBE(0);
+        csv+=Buffer(splitData[j], 'hex').readDoubleBE(0)+',';
+      }
+      csv+='\n';
+      
+    }
+      //console.log(csv);
+      res.attachment('bikeDatas.csv');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(csv, 'utf-8');
+    }
+  });
+});
+
 
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
